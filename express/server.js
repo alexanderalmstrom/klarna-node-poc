1658855c46c1bfd72988dbb2fbbe3d36ce61f3ac
Alexander Almström <info@alexanderalmstrom.com>
2018-12-16 18:03:12 +0100

    Testing cors fix

--- express/server.js
+++ express/server.js
@@ -14,12 +14,16 @@ const credentials = {
   password: process.env.KLARNA_PASSWORD
 }
 
-router.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With")
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST")
-  next()
-})
+if (process.env.NODE_ENV == 'production') {
+  router.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With")
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST")
+    next()
+  })
+} else {
+  router.all('/*', cors())
+}
 
 router.get('/', function (req, res, next) {
   next()
